31adcd0efa0d6b514b109825eac90f3f65ef97af
Ibrahima <iab.jr10@gmail.com>
2023-06-05 02:34:59 -0400

    Added arrays for uppercase letters, lowercase letters, numbers and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -49,7 +49,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var numOfCharacters;
   var includeLowerCase;
@@ -93,14 +93,16 @@ function writePassword() {
     alert("You have chosen to ommit special characters");
   }
 
-  generatePassword() = {
+  function generatePassword() {
     // Creating arrays that include number, uppercase letters, lowercase letter and special characters
-    var num = '0123456789';
-    var upper = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ';
-    var lower = 'abcdefghijklmnopqrstuvwxyz';
-    var spec = ' !"#$%&()*+,-./:;<=>?@[]\{}~`|^_';
+    let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var upper = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Q', 'X', 'Y', 'Z'];
+    var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var spec = ['!', '"', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '¬', '\\', '|', '<', '>', '/', '?', ':', ';', '@', '~', '#', ',', '.', '[', ']',
+    '`', '{', '}', ' ', "'"];
+    
   }
-
+  generatePassword();
 }
 
 // Add event listener to generate button
