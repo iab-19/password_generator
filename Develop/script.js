71a47d70e3ee435e8fc7c052f3b420b659862fd0
Ibrahima <iab.jr10@gmail.com>
2023-06-05 23:15:46 -0400

    Updated code

--- Develop/script.js
+++ Develop/script.js
@@ -43,6 +43,38 @@ function correctNumber() {
 
 }
 
+
+// Creating arrays that include number, uppercase letters, lowercase letter and special characters
+let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var upper = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Q', 'X', 'Y', 'Z'];
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var spec = ['!', '"', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '¬', '\\', '|', '<', '>', '/', '?', ':', ';', '@', '~', '#', ',', '.', '[', ']',
+'`', '{', '}', ' ', "'"];
+// Select a random number from the num array
+function chooseNum() {
+  var randomNum = num[Math.floor(Math.random() * num.length)];
+  return randomNum;
+}
+
+// Select a random letter from the upper array
+function chooseUpper() {
+  var randomUpp = upper[Math.floor(Math.random() * upper.length)]
+    return randomUpp;
+}
+
+// Select a random letter from the lower array
+function chooseLower() {
+  var randomLow = lower[Math.floor(Math.random() * lower.length)]
+  return randomLow;
+}
+
+// Select a random character from the spec array
+function chooseSpec() {
+  var randomSpec = spec[Math.floor(Math.random() * spec.length)]
+  return randomSpec;
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -56,8 +88,6 @@ function writePassword() {
   var includeUpperCase;
   var includeNumber;
   var includeSpecial;
-
-  passwordText.value = password;
 
   correctNumber();
 
@@ -94,16 +124,29 @@ function writePassword() {
   }
 
   function generatePassword() {
-    // Creating arrays that include number, uppercase letters, lowercase letter and special characters
-    let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    var upper = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Q', 'X', 'Y', 'Z'];
-    var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var spec = ['!', '"', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '¬', '\\', '|', '<', '>', '/', '?', ':', ';', '@', '~', '#', ',', '.', '[', ']',
-    '`', '{', '}', ' ', "'"];
-    
+
+    // create an empty array with the size of the password
+    var password = [];
+    for (var i = 0; i < numOfCharacters; i++) {
+      if (includeLowerCase == true){
+        password.push(chooseLower());
+      }
+      if (includeUpperCase == true) {
+        password.push(chooseUpper());
+      }
+      if (includeNumber == true) {
+        password.push(chooseNum());
+      }
+      if (includeSpecial) {
+        password.push(chooseSpec());
+      }
+    }
+    passwordText.value = password.join('');
+    console.log(password);
   }
   generatePassword();
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
